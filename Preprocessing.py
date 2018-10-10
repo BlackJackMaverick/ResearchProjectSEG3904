bb879bd676c60b9acd14b36423ec8c6b969cfe51
Iymen Abdella <iymen-abdella@hotmail.com>
2018-10-10 19:31:12 -0400

    Completed preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,56 +3,65 @@ import csv
 import os
 import pandas as pd
 
-#TODO: make sure that no empty rows are added to the destination CSV file. 
 #copy all the attack types labelled as dos to another CSV file. 
 def CopyAttackRows(SourceDataset, DestDataset):
-    dosAttackTypes = ["Back", "land", "neptune","pod", "smurf", "teardrop"]
-    numberOfAttacks = 0
-    numberOfNormal = 0
+    dosAttackTypes = ["Back", "land", "neptune","pod", "smurf", "teardrop"] #dos attack labels
 
-    #csv reader for the source dataset, and writer for the destination. Rows will be copied
-    SourceReader = csv.reader(open(SourceDataset))
-    DestWriter = csv.writer(open(DestDataset, 'a'), delimiter=',')
-    for row in SourceReader:
-        if row[41] in dosAttackTypes:
-            DestWriter.writerow(row)
-            numberOfAttacks = numberOfAttacks + 1
-        if row[41] == 'normal':  
-            numberOfNormal = numberOfNormal + 1
-    
-    #summary for the total number of attack records and normal records.
-    print ("In total there are:" + SourceReader.count + "records. Of these records there are:" + numberOfAttacks + "number of DOS attack records. Finally there are " + numberOfNormal + "number of  normal records.")
-
-#prints out all the rows to the terminal
+    #csv reader for the source dataset, and writer for the destination. 
+    # Rows will be copied to the destination if they are attack rows.
+    with open(SourceDataset, 'r') as Source:
+        with open(DestDataset, 'w', newline='') as Dest:
+            SourceReader = csv.reader(Source)
+            DestWriter = csv.writer(Dest)
+            for row in SourceReader:
+                if row[41] in dosAttackTypes:
+                    DestWriter.writerow(row) 
+ 
+#prints out all the rows to the terminal. 
 def PrintAll(DatasetRef):
     datasetReader = csv.reader(open(DatasetRef))
     for row in datasetReader:
         print (row)    
 
-#Print all column types and names, how many records in total, how many are Dos and how many are normal. 
-def PrintSummary(DatasetRef):
-
-    pass
-
+#prints number of total records, how many are  labelled as dos, and how many are normal. 
+def PrintSummary(datasetRef):
+    #counters for records
+    normalCount = 0
+    attackCount = 0
+    recordCount = 0
+    dosAttackTypes = ["Back", "land", "neptune","pod", "smurf", "teardrop"]
+    with open(datasetRef, 'r') as csvFile:
+        #iterate through each row in the csv file.
+        datasetReader = csv.reader(csvFile)
+        for row in datasetReader: 
+            if row[41] in dosAttackTypes:
+                attackCount += 1
+            if row[41] == "normal":
+                normalCount += 1
+            recordCount += 1
+    print ("There are a total of " + str(recordCount) + " records of traffic")
+    print ("There are " + str(attackCount) + " attack records")
+    print ("There are " + str(normalCount) + " normal records")
+    
 #the different data sets file names along with their location
 folderPath ='NSLKDDDataset\\NSL_KDD-master\\CSVData\\DOSattacks'
 DatasetPath ='NSLKDDDataset\\NSL_KDD-master\\CSVData'
 Tests = ['\\SmallTrainingSet.csv', '\\20PercentTrainingSet.csv', '\\FullTrainingSet.csv']
 
-#create a csv file to store only the DOS attacks from each of the datasets. Store all these in a folder.  
+#create a folder to store only the DOS attacks from each of the datasets.  
 if not os.path.exists(folderPath):
     os.makedirs(folderPath)
 
+#If the CSV file does not exist then create it. 
 for test in Tests:
     if not os.path.exists(folderPath+test):
-        with open(folderPath+test, 'wb') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',')
+        open(folderPath+test, 'wb')
+#If the CSV file is empty then populate it with the attack rows of the intended dataset.    
+    try:
+        pd.read_csv(folderPath+test)
+    except:
+        CopyAttackRows((DatasetPath+test), (folderPath + test))
 
-#Add all the rows that contain DOS attacks to the appropriate file.
-PrintAll((DatasetPath + Tests[0]))
-
-#check if the CSV file is empty or not. If it is empty then fill it with the attack data. 
-if pd.read_csv((DatasetPath + Tests[0])).empty:
-    CopyAttackRows((DatasetPath + Tests[0]), (folderPath + Tests[0]))
-
-PrintAll((folderPath + '\\' + Tests[0]))
+    #print out the rows and a summary of the number of records. 
+    PrintAll(folderPath+test)
+    PrintSummary(DatasetPath+test)
